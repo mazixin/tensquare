4a515c4150b8ac55fb17f90b9751ac722faa79bc
mazixin <694179453@qq.com>
2021-03-08 10:49:55 +0800

    给公共模块添加了分页类，状态码类，和雪花算法类

--- tensquare_common/src/main/java/entity/PageResult.java
+++ tensquare_common/src/main/java/entity/PageResult.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.List;
+
 /**
  * @description:分页类
  * @projectName:tensquare_parent
@@ -8,5 +10,31 @@ package entity;
  * @createTime:2021/3/8 10:25
  * @version:1.0
  */
-public class PageResult {
+public class PageResult<T> {
+    private Long total;
+    private List<T> rows;
+
+    public PageResult() {
+    }
+
+    public PageResult(Long total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
 }
