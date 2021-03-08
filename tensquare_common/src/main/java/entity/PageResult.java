cd4cf9d10de1e46100a2f285ed4e679e1e006da3
mazixin <694179453@qq.com>
2021-03-08 10:30:25 +0800

    新建工程-->父工程（tensquare_parent）-->子工程（公用模块）tensquare_common

---
+++ tensquare_common/src/main/java/entity/PageResult.java
@@ -0,0 +1,12 @@
+package entity;
+
+/**
+ * @description:分页类
+ * @projectName:tensquare_parent
+ * @see:entity
+ * @author:MartinKing
+ * @createTime:2021/3/8 10:25
+ * @version:1.0
+ */
+public class PageResult {
+}
